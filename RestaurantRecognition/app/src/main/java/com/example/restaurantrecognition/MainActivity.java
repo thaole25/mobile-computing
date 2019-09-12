fa5e30a591891424a07a7c7ab67c831173dd8e48
thaole25 <thao.t.le25@gmail.com>
2019-09-12 19:54:30 +1000

    open camera

--- RestaurantRecognition/app/src/main/java/com/example/restaurantrecognition/MainActivity.java
+++ RestaurantRecognition/app/src/main/java/com/example/restaurantrecognition/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.restaurantrecognition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public Button button;
+
+    static final int REQUEST_IMAGE_CAPTURE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnCamera);
+
+        button.setOnClickListener(new View.OnClickListener(){
+           @Override
+            public void onClick(View view){
+               Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+               if (cameraIntent.resolveActivity(getPackageManager()) != null){
+                   startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+               }
+           }
+        });
     }
 }
