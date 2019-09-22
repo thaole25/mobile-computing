c3cac4cb9e3290c8dc45317ff65004ae81a85d53
Cameron Dempsey <camerontdempsey@gmail.com>
2019-09-22 18:49:15 +1000

    Implement basic in-app camera preview

--- RestaurantRecognition/app/src/main/java/com/example/restaurantrecognition/ui/search/SearchFragment.java
+++ RestaurantRecognition/app/src/main/java/com/example/restaurantrecognition/ui/search/SearchFragment.java
@@ -1,25 +1,40 @@
 package com.example.restaurantrecognition.ui.search;
 
-import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Matrix;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
+import android.util.Rational;
+import android.util.Size;
+import android.view.*;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.restaurantrecognition.R;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.camera.core.*;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-
 import androidx.lifecycle.ViewModelProviders;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import com.example.restaurantrecognition.R;
+
 
 public class SearchFragment extends Fragment {
 
-    @BindView(R.id.btnSearchImage)
-    Button buttonSearchImage;
+    private final int REQUEST_CODE_PERMISSIONS = 10;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA"};
+
+//    @BindView(R.id.btnSearchImage)
+//    Button buttonSearchImage;
+
+    @BindView(R.id.view_finder)
+    TextureView textureView;
+
+    @BindView(R.id.imgCapture)
+    ImageButton imgBtn;
 
     private SearchViewModel searchViewModel;
 
@@ -30,17 +45,110 @@ public class SearchFragment extends Fragment {
         View searchView = inflater.inflate(R.layout.fragment_search, container, false);
         ButterKnife.bind(this, searchView);
 
-        buttonSearchImage.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (cameraIntent.resolveActivity(getActivity().getPackageManager()) != null){
-                    startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
-                }
-            }
-        });
+        if(allPermissionsGranted()){
+            startCamera(); //start camera if permission has been granted by user
+        } else{
+            ActivityCompat.requestPermissions(getActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
 
         return searchView;
+    }
+
+    private void startCamera() {
+        CameraX.unbindAll();
+        Rational aspectRatio = new Rational(textureView.getWidth(), textureView.getHeight());
+        Size screen = new Size(textureView.getWidth(), textureView.getHeight());
+
+        PreviewConfig pConfig = new PreviewConfig.Builder().setTargetAspectRatio(aspectRatio)
+                .setTargetResolution(screen).build();
+        Preview preview = new Preview(pConfig);
+
+        preview.setOnPreviewOutputUpdateListener(
+                output -> {
+                    ViewGroup parent = (ViewGroup) textureView.getParent();
+                    parent.removeView(textureView);
+                    parent.addView(textureView, 0);
+                    textureView.setSurfaceTexture(output.getSurfaceTexture());
+                    updateTransform();
+                }
+        );
+
+        ImageCaptureConfig imageCaptureConfig = new ImageCaptureConfig.Builder().setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+                .setTargetRotation(getActivity().getWindowManager().getDefaultDisplay().getRotation()).build();
+        final ImageCapture imgCap = new ImageCapture(imageCaptureConfig);
+
+       imgBtn.setOnClickListener(v -> {
+           imgCap.takePicture(new ImageCapture.OnImageCapturedListener() {
+               @Override
+               public void onCaptureSuccess(ImageProxy image, int rotationDegrees) {
+                   String msg = "Camera pic taken!";
+                   Toast.makeText(getContext(), msg,Toast.LENGTH_LONG).show();
+                   super.onCaptureSuccess(image, rotationDegrees);
+               }
+
+               @Override
+               public void onError(@NonNull ImageCapture.ImageCaptureError imageCaptureError, @NonNull String message, @Nullable Throwable cause) {
+                   super.onError(imageCaptureError, message, cause);
+               }
+           });
+       });
+        //bind to lifecycle:
+        CameraX.bindToLifecycle(this, preview, imgCap);
+    }
+
+    private void updateTransform() {
+        Matrix mx = new Matrix();
+        float w = textureView.getMeasuredWidth();
+        float h = textureView.getMeasuredHeight();
+
+        float cX = w / 2f;
+        float cY = h / 2f;
+
+        int rotationDgr;
+        int rotation = (int)textureView.getRotation();
+
+        switch(rotation){
+            case Surface.ROTATION_0:
+                rotationDgr = 0;
+                break;
+            case Surface.ROTATION_90:
+                rotationDgr = 90;
+                break;
+            case Surface.ROTATION_180:
+                rotationDgr = 180;
+                break;
+            case Surface.ROTATION_270:
+                rotationDgr = 270;
+                break;
+            default:
+                return;
+        }
+
+        mx.postRotate((float)rotationDgr, cX, cY);
+        textureView.setTransform(mx);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == REQUEST_CODE_PERMISSIONS){
+            if(allPermissionsGranted()){
+                startCamera();
+            } else{
+                Toast.makeText(getContext(), "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+                getActivity().finish();
+            }
+        }
+    }
+
+    private boolean allPermissionsGranted(){
+
+        for(String permission : REQUIRED_PERMISSIONS){
+            if(ContextCompat.checkSelfPermission(getContext(), permission) != PackageManager.PERMISSION_GRANTED){
+                return false;
+            }
+        }
+        return true;
     }
 
 }
