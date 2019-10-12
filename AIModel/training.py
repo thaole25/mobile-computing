932588694a2fb209cb47d4c2cb9f72ba3c254447
thaole25 <thao.t.le25@gmail.com>
2019-10-12 11:25:39 +1100

    save data into numpy array

--- AIModel/training.py
+++ AIModel/training.py
@@ -2,14 +2,18 @@ import os
 from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 import shutil
 import argparse
+import numpy as np 
+import pandas as pd 
 
-IMG_HEIGHT = 512
-IMG_WIDTH = 512
-MAX_IMAGES = 20
+IMG_HEIGHT = 320 #640
+IMG_WIDTH = 160 #320
+MAX_IMAGES = 50
 
 IMAGES_PATH = "../data/training/images/"
 AUGMENTATION_PATH = "../data/training/augmentation-images/"
 CSV_RESTAURANTS = "../data/training/restaurants.csv"
+X_TRAIN_FILE = "../data/training/x_train.npy"
+Y_TRAIN_FILE = "../data/training/y_train.npy"
 
 def preprocess_image(imagePath):
   image = load_img(imagePath, target_size=(IMG_HEIGHT, IMG_WIDTH))
@@ -49,10 +53,41 @@ def image_augmentation():
         for _ in imageGen.flow(image, batch_size=1, save_to_dir=saveDir, save_prefix='new'):
           i += 1
           if i > remainderSize: break 
-    
+
+def create_training_data():
+  restaurantspd = pd.read_csv(CSV_RESTAURANTS, names=['Id','Name','Latitude','Longitude','Address'])
+  restaurantspd['Label'] = restaurantspd['Id'].astype('category')
+  restaurantspd = restaurantspd.set_index('Id')
+  restaurantsDict = restaurantspd.to_dict()
+  restaurantsLabels = restaurantsDict['Label']
+
+  restaurantsIds = [folder[1] for folder in os.walk(IMAGES_PATH)]
+  restaurantsIds = restaurantsIds[0]
+  totalInstances = len(restaurantsIds) * MAX_IMAGES
+  x_train = np.ndarray((totalInstances, IMG_HEIGHT, IMG_WIDTH, 3), dtype=np.float32)
+  y_train = np.ndarray((totalInstances, ), dtype=np.uint8)
+  i = 0
+  for restaurantsId in restaurantsIds:
+    restaurantPath = IMAGES_PATH + restaurantsId + '/'
+    for res in os.walk(restaurantPath):
+      imageFiles = res[2]
+      for imageFile in imageFiles:
+        image = load_img(restaurantPath + imageFile, target_size=(IMG_HEIGHT, IMG_WIDTH))
+        image = img_to_array(image)
+        x_train[i] = image
+        y_train[i] = restaurantsLabels[int(restaurantsId)]
+        i += 1
+  np.save(X_TRAIN_FILE, x_train)
+  np.save(Y_TRAIN_FILE, y_train)
+
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("-a", "--augmentation", help="", action="store_true")
+  parser.add_argument("-c", "--create_training", help="", action="store_true")
+  
   args = parser.parse_args()
   if args.augmentation:
     image_augmentation()
+  if args.create_training:
+    create_training_data()
