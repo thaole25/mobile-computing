dc8e89b5b3d53eec7361682a1fe4211af262e807
thaole25 <thao.t.le25@gmail.com>
2019-10-17 16:18:14 +1100

    create tflite model

--- AIModel/main.py
+++ AIModel/main.py
@@ -12,12 +12,20 @@ import argparse
 import preprocessing
 import model
 import testing
+import constants
+
+def convert_to_tensorflowLite():
+  converter = tf.lite.TFLiteConverter.from_keras_model_file(constants.BEST_MODEL_MOBILENET)
+  tfliteModel = converter.convert()
+  f = open("mobilenet_tflite", "wb")
+  f.write(tfliteModel)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("-pre", "--preprocessingImages", help="", action="store_true")
   parser.add_argument("-train", "--training", help="", action="store_true")
   parser.add_argument("-test", "--runtesting", help="", action="store_true")
+  parser.add_argument("-conv", "--convert", help="", action="store_true")
   
   args = parser.parse_args()
   if args.preprocessingImages:
@@ -27,5 +35,7 @@ if __name__ == "__main__":
     model.run_mobilenet()
   if args.runtesting:
     testing.testing()
+  if args.convert:
+    convert_to_tensorflowLite()
 
     
