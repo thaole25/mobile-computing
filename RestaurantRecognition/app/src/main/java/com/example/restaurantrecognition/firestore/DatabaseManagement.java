b921f37497a96674436cc6e3f12298bd3fe73dd0
gon009 <gegarciam95@gmail.com>
2019-10-20 18:40:33 +1100

    Read data from firestore

---
+++ RestaurantRecognition/app/src/main/java/com/example/restaurantrecognition/firestore/DatabaseManagement.java
@@ -0,0 +1,64 @@
+package com.example.restaurantrecognition.firestore;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class DatabaseManagement {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference restaurantRefList = db.collection("restaurants");
+
+    // Read the document: restaurants from firestore and save them in a arraylist.
+    public ArrayList<Restaurant> readData(FirestoreCallBack fireStoreCallback){
+        ArrayList<Restaurant> restaurantList = new ArrayList<>();
+        restaurantRefList.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DocumentSnapshot document : task.getResult()) {
+                        Restaurant restaurant = new Restaurant(document.getId(),null,null,0,0,null);
+                        restaurantList.add(restaurant);
+                    }
+                    fireStoreCallback.onCallBack(restaurantList);
+                }
+                else
+                {
+                    Log.d("Error", "Error getting documents.", task.getException());
+                }
+            }
+        });
+        return restaurantList;
+    }
+
+    // Firestore works asynchronously, so it does not retrieve the data correctly and return [].
+    // Therefore I found this solution is necessary using a interface callback
+    public interface FirestoreCallBack{
+        void onCallBack(ArrayList<Restaurant> restaurantArrayList);
+    }
+
+    // Example to retrieve data , use this code wherever you want to get the data
+    public ArrayList<Restaurant> getAllData() {
+
+        //db.readData(restaurantArrayList -> Log.d("Size of array: ", String.valueOf(restaurantArrayList.size())));
+
+        ArrayList<Restaurant> restaurants = new ArrayList<>();
+        readData(new FirestoreCallBack() {
+            @Override
+            public void onCallBack(ArrayList<Restaurant> restaurantArrayList) {
+                Log.d("Result", String.valueOf(restaurantArrayList.size()));
+            }
+        });
+
+        return restaurants;
+    }
+}
