9907c9ed02d49172fd36a3106d8059b67202eccb
Cameron Dempsey <camerontdempsey@gmail.com>
2019-10-22 23:01:24 +1100

    Add border to Recent Matches list items and dummy matches

--- RestaurantRecognition/app/src/main/java/com/example/restaurantrecognition/SideMenuActivity.java
+++ RestaurantRecognition/app/src/main/java/com/example/restaurantrecognition/SideMenuActivity.java
@@ -1,5 +1,7 @@
 package com.example.restaurantrecognition;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -23,6 +25,9 @@ import com.example.restaurantrecognition.ui.recentmatches.RecentMatchesFragment;
 import com.example.restaurantrecognition.ui.search.SearchFragment;
 import com.example.restaurantrecognition.ui.settings.SettingsFragment;
 import com.google.android.material.navigation.NavigationView;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 public class SideMenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, RecentMatchesFragment.OnListFragmentInteractionListener {
@@ -32,6 +37,19 @@ public class SideMenuActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(RecentMatchItem.PREFERENCES_STORE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        ArrayList<RecentMatchItem> matchItemList = new ArrayList<>();
+        matchItemList.add(new RecentMatchItem(1, "Prontos", 324223));
+        matchItemList.add(new RecentMatchItem(2, "Sushi Place", 165164));
+        matchItemList.add(new RecentMatchItem(3, "Catch of the Day", 596111));
+        matchItemList.add(new RecentMatchItem(4, "Subway", 165151));
+        String json = gson.toJson(matchItemList);
+        editor.putString(RecentMatchItem.PREFERENCES_STORE_NAME, json);
+        editor.apply();
+
         setContentView(R.layout.activity_side_menu);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
