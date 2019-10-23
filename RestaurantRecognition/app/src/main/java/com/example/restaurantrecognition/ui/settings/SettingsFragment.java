ae8c460d1faf64f2df741f012d73b4564a00513e
Cameron Dempsey <camerontdempsey@gmail.com>
2019-10-23 19:32:10 +1100

    Add "Clear Recent Matches" button to Settings fragment

--- RestaurantRecognition/app/src/main/java/com/example/restaurantrecognition/ui/settings/SettingsFragment.java
+++ RestaurantRecognition/app/src/main/java/com/example/restaurantrecognition/ui/settings/SettingsFragment.java
@@ -1,34 +1,41 @@
 package com.example.restaurantrecognition.ui.settings;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.restaurantrecognition.R;
+import com.example.restaurantrecognition.ui.recentmatches.RecentMatchItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SettingsFragment extends Fragment {
 
     private SettingsViewModel settingsViewModel;
+
+    @BindView(R.id.btnClearRecentMatches)
+    Button btnClearRecentMatches;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         settingsViewModel =
                 ViewModelProviders.of(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        final TextView textView = root.findViewById(R.id.text_settings);
-        settingsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
+        ButterKnife.bind(this, root);
+        btnClearRecentMatches.setOnClickListener(view -> {
+            SharedPreferences sharedPreferences = getContext().getSharedPreferences(RecentMatchItem.PREFERENCES_STORE_NAME, Context.MODE_PRIVATE);
+            sharedPreferences.edit().clear().commit();
+            Toast.makeText(getContext(), "Cleared all saved recent matches", Toast.LENGTH_SHORT).show();
         });
         return root;
     }
